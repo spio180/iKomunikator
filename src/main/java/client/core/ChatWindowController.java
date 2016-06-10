d69f9b7ad4ab3e348ecb0b40b1860fbcb753d7e0
Lukasz Wozniak <lukasz.wozniak.pl@gmail.com>
2016-06-10 12:44:10 +0200

    Added Client source code

---
+++ src/main/java/client/core/ChatWindowController.java
@@ -0,0 +1,51 @@
+package client.core;
+
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+
+
+public class ChatWindowController {
+
+	@FXML private Button butWyczysc;
+	@FXML public Button butWyslij;
+	@FXML public ListView<String> textChat;
+	@FXML private TextArea textToSend;
+	@FXML private ListView<String> userList;
+
+	private TcpClient tcpConnectionToServer;
+	//protected List<String> listOfCom = new ArrayList<>();
+
+
+	public void setTcpConnectionToServer(TcpClient connection){
+		tcpConnectionToServer = connection;
+	}
+
+	@FXML private void butWyczyszClick(){
+		textToSend.clear();
+
+	}
+
+
+	@FXML private void butWyslijClick(){
+
+		String msg = textToSend.getText();
+
+		if (msg.length() != 0)
+		{
+			tcpConnectionToServer.sendMessage(textToSend.getText());
+			tcpConnectionToServer.listOfCom.add(textToSend.getText());
+
+			System.out.println(tcpConnectionToServer.listOfCom.size());
+
+			textChat.setItems(FXCollections.observableArrayList(tcpConnectionToServer.listOfCom));
+			textChat.scrollTo(tcpConnectionToServer.listOfCom.size()-1);
+			textToSend.clear();
+		}
+	}
+
+}
---
+++ src/main/java/client/core/LogWindowController.java
@@ -0,0 +1,116 @@
+package client.core;
+
+import java.io.IOException;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+public class LogWindowController {
+	@FXML private Button butCancel;
+	@FXML private Button butConnect;
+	@FXML private TextField textClientIP;
+	@FXML TextField userLogin;
+	@FXML Pane tcpClientPort;
+	@FXML TextField textClientPort;
+	private TcpClient connectionToServer = null;
+
+	@FXML private void closeButtonAction(){
+
+		/* Closing connection */
+		if (connectionToServer != null) {
+			connectionToServer.terminateListenningThread();
+			connectionToServer.closeSocket();
+			System.out.println("Connection to server closed");
+		}
+		// get a handle to the stage
+	    Stage stage = (Stage) butCancel.getScene().getWindow();
+
+	    // do what you have to do
+	    stage.close();
+	}
+
+	@FXML private void connectButtonAction() {
+
+		connectionToServer = new TcpClient();
+		butConnect.setDisable(true);
+
+
+		/*connection establishing */
+		int rc = connectionToServer.connectToServer(textClientIP.getText(),
+					Integer.parseInt(textClientPort.getText()));
+
+
+		if (rc != 0) {
+			System.out.println("Cbutton");
+			butConnect.setDisable(true);
+			return;
+		}
+
+
+		/*sending logging message */
+		/*HashMap<String, String> loginPayload = new HashMap<String, String>();
+		loginPayload.put("LOGIN", userLogin.getText());
+		Message loginMessage = new Message("LOGIN_REQ", userLogin.getText(),
+				"##", loginPayload);
+		System.out.println(Serialization.SerializeMessage(loginMessage));
+		connectionToServer.sendMessage(Serialization.SerializeMessage(loginMessage));
+		*/
+
+		connectionToServer.sendMessage(userLogin.getText());
+		/*verify answear from server */
+
+		if (true) { /*check nee to be implemented*/
+			//Starting chat window
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ChatWindow.fxml"));
+			BorderPane chatWindow;
+
+			try {
+
+				chatWindow = (BorderPane) loader.load();
+				Stage stageChat = new Stage();
+		        stageChat.setTitle("iKomunikator");
+		        stageChat.setScene(new Scene(chatWindow, 480, 820));
+		        stageChat.show();
+
+		        stageChat.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		            public void handle(WindowEvent we) {
+		                System.out.println("Stage is closing");
+		                if (connectionToServer != null) {
+		        			connectionToServer.terminateListenningThread();
+		        			connectionToServer.closeSocket();
+		        			System.out.println("Connection to server closed");
+		        		}
+		            }
+		        });
+
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Chat Window could not be created");
+				e.printStackTrace();
+				return;
+			}
+
+			//setting TCP connection for ChatWindow
+			ChatWindowController chatController = (ChatWindowController) loader.getController();
+			chatController.setTcpConnectionToServer(connectionToServer);
+			connectionToServer.setChatController(chatController);
+
+			// get a handle to the stage
+		    Stage stage = (Stage) butConnect.getScene().getWindow();
+
+		    butConnect.setDisable(true);
+		    // do what you have to do
+		    stage.hide();
+		}
+
+	}
+}
