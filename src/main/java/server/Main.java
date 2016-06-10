6cda6f2aaa8f85c8dddfc590a189db2858b6aadc
Lukasz Wozniak <lukasz.wozniak.pl@gmail.com>
2016-06-10 17:04:02 +0200

    Server Termination Method

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,9 +4,11 @@ import common.ServerConfig;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import server.core.Server;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static final Boolean DEBUG = Boolean.TRUE;
+    public static final Boolean DEBUG = Boolean.FALSE;
 
     /**
      * @param args no args required
@@ -16,6 +18,11 @@ public class Main {
         Server server = Server.getInstance();
         ServerConfig conf = server.getServerConfig();
         System.out.printf("Server address %s:%d | Limit %d\n", conf.getServerIp(), conf.getPortNumber(), conf.getConnectionLimit());
+
+        System.out.println("Press return to turn off the server: ");
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine();
+        server.stopServer();
     }
 }
 
