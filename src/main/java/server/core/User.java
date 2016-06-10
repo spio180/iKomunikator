36a3e4e37ca56b196d6f97bd3022ce8d5c430db1
Lukasz Wozniak <lukasz.wozniak.pl@gmail.com>
2016-06-10 17:01:33 +0200

    Added Used Class getters and setters

--- src/main/java/server/core/User.java
+++ src/main/java/server/core/User.java
@@ -4,7 +4,25 @@ package server.core;
  * Created by lukasz on 10.06.16.
  */
 public class User {
-    private String nick;
-    private Integer connectionId;
-    private String Status;
+    private String mNick;
+    private Integer mConnectionId;
+    private String mStatus;
+
+    public User(String nick, Integer connectionId, String status) {
+        mNick = nick;
+        mConnectionId = connectionId;
+        mStatus = status;
+    }
+
+    public String getNick() {
+        return mNick;
+    }
+
+    public Integer getConnectionId() {
+        return mConnectionId;
+    }
+
+    public String getStatus() {
+        return mStatus;
+    }
 }
