42dc0f5958a216f5702d1e38af91b3691f7d9608
Lukasz Wozniak <lukasz.wozniak.pl@gmail.com>
2016-06-11 15:32:29 +0200

    Connection Class routes the messages.

--- src/main/java/server/core/Connection.java
+++ src/main/java/server/core/Connection.java
@@ -1,9 +1,11 @@
 package server.core;
 
+import common.Const;
 import common.Message;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Connection implements Runnable {
     private final Integer mConnectionId;
@@ -11,33 +13,55 @@ public class Connection implements Runnable {
     private final Socket mClientSocket;
     private final Receiver mListener;
     private final Transmitter mTransmitter;
+    private UserList mUserList;
 
 
     public Connection(Server server, Socket clientSocket) {
         mServer = server;
         mClientSocket = clientSocket;
         mConnectionId = mClientSocket.getPort();
-        mListener = new Receiver(this, mClientSocket);
-        mTransmitter = new Transmitter(this, mClientSocket);
+        mListener = new Receiver(this);
+        mTransmitter = new Transmitter(this);
+        mUserList = mServer.getRouter().getUserList();
 
     }
 
     public void run() {
         mServer.addConnection(this);
-        System.out.print("New Client Connected: " + mServer.getConnectionList().size());
+        System.out.println("New Client Connected: " + mServer.getConnectionList().size());
         Message message = null;
         while (true) {
             try {
                 Thread.sleep(10L);
                 message = mListener.read();
                 if (message != null) {
-                    if(mServer.getUserList().userIsConnected(message.getSender()) == false){
-                        User newUser = new User(message.getSender(), mConnectionId, Status.ONLINE);
-                        mServer.getUserList().addUser(newUser);
+                    if (mUserList.userIsConnected(message.getSender()) == false) {
+                        User newUser = new User(message.getSender(), this, Status.ONLINE);
+                        mUserList.addUser(newUser);
                     } else {
                         //TODO: Ping if user is still active.
                     }
-                    new RouteMessage(message);
+                    if (Const.MSG_LOGOWANIE.equals(message.getType())) {
+                        HashMap<String, String> bodyMessage = new HashMap<String, String>();
+                        bodyMessage.put("response", "Poprawnie zalogowano.");
+                        Message response = new Message(Const.MSG_LOGOWANIE_OK, Const.USER_SERVER, message.getSender(), bodyMessage);
+                        mTransmitter.sendBack(response);
+                    } else if (Const.MSG_DO_WSZYSTKICH.equals(message.getType())) {
+                        HashMap<String, String> bodyMessage = new HashMap<String, String>();
+                        bodyMessage.put(Const.BODY, message.getMessageBody().get(Const.BODY));
+                        Message response = new Message(Const.MSG_DO_WSZYSTKICH, message.getSender(), Const.USER_ALL , bodyMessage);
+                        mTransmitter.sendToAll(response);
+                    } else if (Const.MSG_DO_UZYTKOWNIKA.equals(message.getType())) {
+                        HashMap<String, String> bodyMessage = new HashMap<String, String>();
+                        bodyMessage.put(Const.BODY, message.getMessageBody().get(Const.BODY));
+                        Message response = new Message(Const.MSG_DO_WSZYSTKICH, message.getSender(), Const.USER_ALL , bodyMessage);
+                        mTransmitter.sendTo(message.getReceiver(),response);
+                    } else {
+                        HashMap<String, String> bodyMessage = new HashMap<String, String>();
+                        bodyMessage.put("response", "Błędny komunikat.");
+                        Message response = new Message(Const.MSG_LOGOWANIE_OK, Const.USER_SERVER, message.getSender(), bodyMessage);
+                        mTransmitter.sendBack(response);
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
