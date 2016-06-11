94763ba537a169b0d379eb772a3c49127a913fa8
Lukasz Wozniak <lukasz.wozniak.pl@gmail.com>
2016-06-11 15:34:35 +0200

    Serialization class endline added to generated string.

--- src/main/java/common/Serialization.java
+++ src/main/java/common/Serialization.java
@@ -1,116 +1,12 @@
 package common;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.UnknownHostException;
-import java.nio.charset.Charset;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import java.io.InputStream;
 
 public class Serialization {
-//    public static Boolean SerializeServerConfig(ServerConfig config_to_serialize, String config_file_path) {
-//        Boolean result = false;
-//
-//        try {
-//            DocumentBuilderFactory documentBuilderFactory= DocumentBuilderFactory.newInstance();
-//            DocumentBuilder documentBuilder= documentBuilderFactory.newDocumentBuilder();
-//            Document document= documentBuilder.newDocument();
-//            Element element= document.createElement("ServerConfig");
-//            document.appendChild(element);
-//
-//            Element ip = document.createElement("IP");
-//            ip.appendChild(document.createTextNode(config_to_serialize.getServerIp().toString()));
-//            element.appendChild(ip);
-//
-//            Element port = document.createElement("ServerPort");
-//            port.appendChild(document.createTextNode(Integer.toString(config_to_serialize.getPortNumber())));
-//            element.appendChild(port);
-//
-//            Element connectionLimit = document.createElement("ConnectionsLimit");
-//            connectionLimit.appendChild(document.createTextNode(Integer.toString(config_to_serialize.getConnectionLimit())));
-//            element.appendChild(connectionLimit);
-//
-//            TransformerFactory transformerFactory= TransformerFactory.newInstance();
-//            Transformer transformer= transformerFactory.newTransformer();
-//            DOMSource source = new DOMSource(document);
-//
-//            StreamResult streamResult = new StreamResult(new File(config_file_path));
-//            transformer.transform(source, streamResult);
-//        }
-//        catch(Exception ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//
-//        return result;
-//    }
-//
-//    public static Boolean DeserializeServerConfig(String config_file_path, ServerConfig configuration) throws UnknownHostException, ParserConfigurationException, SAXException {
-//        Boolean result = false;
-//        configuration = new ServerConfig();
-//
-//        try
-//        {
-//            DocumentBuilderFactory documentBuilderFactory= DocumentBuilderFactory.newInstance();
-//            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-//            Document document = documentBuilder.parse(config_file_path);
-//
-//            NodeList ipNodeList = document.getElementsByTagName("IP");
-//            for (int i=0; i< ipNodeList.getLength(); i++) {
-//                Node node = ipNodeList.item(i);
-//
-//                if (node.getNodeType() == Node.ELEMENT_NODE) {
-//                    Element element = (Element) node;
-//                    configuration.setServerIp(element.getNodeValue());
-//                }
-//            }
-//
-//            NodeList serverPortNodeList = document.getElementsByTagName("SERVERPORT");
-//            for (int i=0; i< serverPortNodeList.getLength(); i++) {
-//                Node node = serverPortNodeList.item(i);
-//
-//                if (node.getNodeType() == Node.ELEMENT_NODE) {
-//                    Element element = (Element) node;
-//                    configuration.setPortNumber(Integer.parseInt(element.getNodeValue()));
-//                }
-//            }
-//
-//            NodeList connectionsLimitNodeList = document.getElementsByTagName("CONNECTIONSLIMIT");
-//            for (int i=0; i< connectionsLimitNodeList.getLength(); i++) {
-//                Node node = connectionsLimitNodeList.item(i);
-//
-//                if (node.getNodeType() == Node.ELEMENT_NODE) {
-//                    Element element = (Element) node;
-//                    configuration.setConnectionLimit(Integer.parseInt(element.getNodeValue()));
-//                }
-//            }
-//
-//            result = true;
-//        }
-//        catch(IOException ex) {
-//            return false;
-//        }
-//
-//        return result;
-//    }
 
     public static String SerializeMessage(Message msg) {
         String result;
@@ -120,7 +16,7 @@ public class Serialization {
         xmlEncoder.close();
         result = baos.toString();
         result = result.replace("\\n", "").replace("\n", "");
-        return result;
+        return result + "\n";
     }
 
     public static Message DeSerializeMessage(String msg) {
